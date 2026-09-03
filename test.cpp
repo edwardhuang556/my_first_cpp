8252878fc317f274aa63d06cbe1de8ec8211eb7d
edwardhuang556 <edwardhuang0409@gmail.com>
2026-09-03 18:22:50 +0800

    clean

--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout << "HelloWorld" << endl;
-    cout << "Try modifying this" << endl;
-    cout << "This is a test" << endl;
-    return 0;
-}
